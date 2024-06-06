25fd2670d0e392552af8dc290b191797aaea08b5
Wayne Luke <wayneluke@me.com>
2024-06-06 14:29:05 -0700

    Add charset

--- includes/database.php
+++ includes/database.php
@@ -30,9 +30,9 @@ class Database extends PDO
         if (!$settings = parse_ini_file($file, TRUE)) throw new exception('Unable to open ' . $file . '.');
        
         $dns = $settings['database']['driver'] .
-        ':host=' . $settings['database']['host'] .
-        ((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '') .
-        ';dbname=' . $settings['database']['schema'];
+            ':host=' . $settings['database']['host'] .
+            ((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '') .
+            ';dbname=' . $settings['database']['schema'] . ';charset=' . $settings['database']['charset'];
        
         parent::__construct($dns, $settings['database']['username'], $settings['database']['password']);
     }
